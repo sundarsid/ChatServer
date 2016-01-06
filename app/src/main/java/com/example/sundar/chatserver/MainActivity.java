71a7121c96f8f18454a89a3d6a59c7e42748189d
sundarsid <sundarspace1@gmail.com>
2016-01-06 11:35:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sundar/chatserver/MainActivity.java
@@ -0,0 +1,328 @@
+package com.example.sundar.chatserver;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+public class MainActivity extends ActionBarActivity {
+
+    static final int SocketServerPORT = 8080;
+
+    LinearLayout loginPanel, chatPanel;
+
+    TextView infoIp, infoPort, chatMsg;
+
+    String msgLog = "";
+
+    EditText editTextSay;
+
+
+    Button buttonSend;
+    Button buttonDisconnect;
+    ChatClientThread chatClientThread = null;
+
+
+    ServerSocket serverSocket;
+
+    boolean flag = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginPanel = (LinearLayout)findViewById(R.id.loginpanel);
+        chatPanel = (LinearLayout)findViewById(R.id.chatpanel);
+        infoIp = (TextView) findViewById(R.id.infoip);
+        infoPort = (TextView) findViewById(R.id.infoport);
+        chatMsg = (TextView) findViewById(R.id.chatmsg);
+        buttonSend = (Button)findViewById(R.id.send);
+        buttonDisconnect = (Button) findViewById(R.id.disconnect);
+
+        infoIp.setText(getIpAddress());
+
+        editTextSay = (EditText)findViewById(R.id.say);
+
+        buttonSend.setOnClickListener(buttonSendOnClickListener);
+        buttonDisconnect.setOnClickListener(buttonDisconnectOnClickListener);
+
+
+
+
+        ChatServerThread chatServerThread = new ChatServerThread();
+        chatServerThread.start();
+    }
+
+    View.OnClickListener buttonDisconnectOnClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if(chatClientThread==null){
+                return;
+            }
+            chatClientThread.disconnect();
+        }
+
+    };
+
+    View.OnClickListener buttonSendOnClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if (editTextSay.getText().toString().equals("")) {
+                return;
+            }
+
+            if(chatClientThread==null){
+                return;
+            }
+
+            chatClientThread.sendMsg(editTextSay.getText().toString() + "\n");
+            editTextSay.setText("");
+        }
+
+    };
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class ChatServerThread extends Thread {
+
+        @Override
+        public void run() {
+            Socket socket = null;
+
+            try {
+                serverSocket = new ServerSocket(SocketServerPORT);
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        infoPort.setText("Port No: "
+                                + serverSocket.getLocalPort());
+                    }
+                });
+
+                while (!flag) {
+                    socket = serverSocket.accept();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            loginPanel.setVisibility(View.GONE);
+                            chatPanel.setVisibility(View.VISIBLE);
+                        }
+
+                    });
+
+                  chatClientThread= new ChatClientThread(socket);
+                    chatClientThread.start();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    private class ChatClientThread extends Thread {
+
+
+        String msgToSend = "";
+        boolean goOut = false;
+        Socket socket = null;
+
+        ChatClientThread(Socket socket) {
+            this.socket = socket;
+
+        }
+
+        @Override
+        public void run() {
+
+            DataOutputStream dataOutputStream = null;
+            DataInputStream dataInputStream = null;
+
+            try {
+                dataOutputStream = new DataOutputStream(
+                        socket.getOutputStream());
+                dataInputStream = new DataInputStream(socket.getInputStream());
+
+
+                while (!goOut) {
+                    if (socket != null) {
+                        if (dataInputStream.available() > 0) {
+                            msgLog += dataInputStream.readUTF();
+
+                            MainActivity.this.runOnUiThread(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    chatMsg.setText(msgLog);
+                                }
+                            });
+                        }
+
+                        if (!msgToSend.equals("")) {
+                            dataOutputStream.writeUTF(msgToSend);
+                            dataOutputStream.flush();
+                            msgToSend = "";
+                        }
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this,
+                                "Client left", Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                }
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+                final String eString = e.toString();
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this,
+                                "Client left", Toast.LENGTH_LONG).show();
+                    }
+
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+                final String eString = e.toString();
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this,
+                                "Client left", Toast.LENGTH_LONG).show();
+                    }
+
+                });
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataOutputStream != null) {
+                    try {
+                        dataOutputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+
+                        loginPanel.setVisibility(View.VISIBLE);
+                        chatPanel.setVisibility(View.GONE);
+                    }
+
+                });
+            }
+
+        }
+
+        private void sendMsg(String msg){
+            msgToSend = msg;
+        }
+
+        private void disconnect(){
+            goOut = true;
+
+        }
+    }
+
+    private String getIpAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "My Ip "
+                                + inetAddress.getHostAddress() + "\n";
+                    }
+
+                }
+
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+
+        return ip;
+    }
+
+
+
+}
